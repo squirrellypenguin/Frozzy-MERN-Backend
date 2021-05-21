8afbffa7da10b9c122858ebaac1ff93996613258
Ted Teah <from@dabronx.nyc>
2021-05-20 21:46:51 -0400

    define models

---
+++ models/Creem.js
@@ -0,0 +1,23 @@
+// Import Mongoose
+const mongoose = require("mongoose");
+
+// Pull Schema and model from mongoose
+const Schema = mongoose.Schema;
+const model = mongoose.model;
+
+// Create Place Schema
+const creemSchema = new Schema({
+  	name: String,
+	description: String,
+	img: String,
+	cost: Number,
+	rating: Number
+}, {
+    timestamps: true
+});
+
+// Create our Model Object
+const Creem = model("Creem", creemSchema);
+
+// Export our Model Object
+module.exports = Creem;
---
+++ models/Store.js
@@ -0,0 +1,23 @@
+// Import Mongoose
+const mongoose = require("mongoose");
+
+// Pull Schema and model from mongoose
+const Schema = mongoose.Schema;
+const model = mongoose.model;
+
+// Create Place Schema
+const storeSchema = new Schema({
+  		name: String,
+		descirption: String,
+		img: String,
+		location: String,
+		rating: Number
+}, {
+    timestamps: true
+});
+
+// Create our Model Object
+const Store = model("Store", storeSchema);
+
+// Export our Model Object
+module.exports = Store;
